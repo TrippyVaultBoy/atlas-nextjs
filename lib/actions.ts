d2908bb22d8128a887d9a3e8889ebf269930069e
Tanner Saint <tsaint2004@gmail.com>
2025-10-02 00:28:45 +0000

    added ask question function

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { insertTopic } from "./data";
+import { insertQuestion, insertTopic } from "./data";
 import { redirect } from "next/navigation";
 
 export async function addTopic(data: FormData) {
@@ -16,5 +16,19 @@ export async function addTopic(data: FormData) {
   } finally {
     revalidatePath("/ui/topics/[id]", "page");
     topic && redirect(`/ui/topics/${topic.id}`);
+  }
+}
+
+export async function addQuestion(question: FormData) {
+  try {
+    insertQuestion({
+      title: question.get("title") as string,
+      topic_id: question.get("topic_id") as string,
+      votes: 0,
+    });
+    revalidatePath("/ui/topics/[id]", "page");
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to add question.");
   }
 }
